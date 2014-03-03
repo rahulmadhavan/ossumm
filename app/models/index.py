5a87a4a17c78c5b5b4d0a1a1e4fc6f95430d287e
rahulm <rahulm@flipkart.com>
2014-03-03 16:15:12 +0530

    removed hardcoded values for port, hostname and base folder

--- app/models/index.py
+++ app/models/index.py
@@ -1,31 +1,76 @@
 from flask import Flask, send_from_directory
 from image_generator import ImageGenerator
-from flask import render_template
+from flask import render_template, redirect
+import sys
 
-app = Flask(import_name = "ossumm",template_folder = "/Users/rahulm/Documents/Developer/Projects/ossumm/app/models/public")
+# -i ip , -p port, -b base_folder, -h hostname
+
+pargs = {
+    '-i' : '127.0.0.1',
+    '-p' : 5000,
+    '-b' : '/home/ubuntu',
+    '-h' : 'localhost'
+}
+
+
+if len(sys.argv) % 2 == 0 :
+    raise RuntimeError('arguments are not specified correctly -i ip , -p port, -b base_folder, -h hostname')
+
+option = None
+command = None
+
+for arg in sys.argv:
+    if command is None:
+        command = arg
+    else:
+        if option is None:
+            if arg not in ['-i','-p','-h','-b']:
+                raise RuntimeError('options are not specified correctly '+ arg)
+            else:
+                option = arg
+        else:
+            if arg in ['-i','-p','-h','-b']:
+                raise RuntimeError('options are not specified correctly '+ arg)
+            pargs[option] = arg
+            option = None
+
+
+
+IP = pargs['-i']
+PORT = int(pargs['-p'])
+BASE_FOLDER = pargs['-b']
+HOST_NAME = pargs['-h']
+if PORT == 80:
+    BASE_URL = "http://"+HOST_NAME+"/"
+else:
+    BASE_URL = "http://"+HOST_NAME+":"+str(PORT)+"/"
+
+
+
+app = Flask(import_name = "ossumm",template_folder = BASE_FOLDER+"/ossumm/app/models/public")
 app.debug = True
 
 
-@app.route("/gen/css/<path:filename>")
+@app.route("/css/<path:filename>")
 def css(filename):
-   return send_from_directory("/Users/rahulm/Documents/Developer/Projects/ossumm/app/models/public/css",filename)
+   return send_from_directory(BASE_FOLDER+"/ossumm/app/models/public/css",filename)
 
-@app.route("/gen/images/<path:filename>")
+@app.route("/images/<path:filename>")
 def images(filename):
-   return send_from_directory("/Users/rahulm/Documents/Developer/Projects/ossumm/app/models/public/images",filename)
+   return send_from_directory(BASE_FOLDER+"/ossumm/app/models/public/images",filename)
 
-@app.route("/gen/js/<path:filename>")
+@app.route("/js/<path:filename>")
 def js(filename):
-   return send_from_directory("/Users/rahulm/Documents/Developer/Projects/ossumm/app/models/public/js",filename)
+   return send_from_directory(BASE_FOLDER+"/ossumm/app/models/public/js",filename)
 
 
-@app.route("/gen/<search>", methods=['GET', 'POST'] )
+@app.route("/<search>", methods=['GET', 'POST'] )
 def gen(search):
     images = ImageGenerator().generate(search)
     simages=[]
     for image in images:
         simages.append(unicode(image.get_html(),"utf8"))
-    return render_template('search-result-template.html',images = simages)
+    return render_template('search-result-template.html',images = simages, base_url = BASE_URL)
 
 @app.route("/gens/<search>", methods=['GET', 'POST'])
 def gens(search):
@@ -41,8 +86,12 @@ def temp():
 
 @app.route("/")
 def hello():
+    return redirect(BASE_URL+'dog',302)
+
+@app.route("/healthcheck")
+def healthcheck():
     return "Hello World! yay!!!"
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run(IP,PORT)
